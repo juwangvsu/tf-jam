07bf54e4e5f6525db616a76c66bfc3b5c9aa4937
student <jwang@vsu.edu>
2020-02-22 15:09:13 -0500

    tsjs_to_keras.sh

--- tsjs/build.sh
+++ tsjs/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 node index.js
-bash tsjs_to_keras.sh
+#bash tsjs_to_keras.sh
+tensorflowjs_converter     --input_format tfjs_layers_model --output_format keras ./model_as_tsjs/model.json ./model_as_keras/model.h5
+
 python3 keras_to_pb.py
 cp frozen2.pb.bytes ../Assets/Resources/frozen2.pb.bytes
 python3 pb_to_tensorboard.py
+
 #python3 /usr/local/lib/python3.5/dist-packages/tensorflow/python/tools/import_pb_to_tensorboard.py  --model_dir ./frozen2.pb.bytes --log_dir log3
 #tensorboard --logdir log3
 #bash pb_to_frozen_pb.sh
--- tsjs/tsjs_to_keras.sh
+++ tsjs/tsjs_to_keras.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+# in win10, an 0x0d might be inserted at the end of line and cause bash problem
 tensorflowjs_converter     --input_format tfjs_layers_model --output_format keras ./model_as_tsjs/model.json ./model_as_keras/model.h5
